e856df0f1db91efb015b5a8fd0e89d8c43a0d730
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-09-26 02:42:34 -0400

    drafts

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
+import os
+
+from tinydb import TinyDB
+
+# import pathlib
+from config import settings
+
 if __name__ == '__main__':
-    print('hello')
+
+    data_folder = settings.data_folder
+    explain_json_format = settings.explain_json_format
+
+    if not os.path.exists(os.path.join(os.getcwd(), data_folder)):
+        os.mkdir(os.path.join(os.getcwd(), data_folder))
+
+    db_filename = os.path.join(
+        os.getcwd(),
+        data_folder,
+        (
+            os.environ.get('USER')
+            + '.abaco'
+            + ('.json' if explain_json_format is True else '')
+        ),
+    )
+
+    # if not pathlib.Path.is_file(db_filename):
+    db = TinyDB(db_filename)
+    db.default_table_name = 'teste_eilliam'
+
+    db.insert(
+        {'user_config': {'name': 'William Sampaio', 'language': 'pt-BR'}}
+    )
