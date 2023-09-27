c4c7a896cedd8f05b22a8a3f2c8c5ffb1a440e23
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-09-27 04:09:08 -0400

    fix

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from flaskwebgui import FlaskUI
-from rich import print as p
 
 from abaco import create_app
 
 if __name__ == '__main__':
 
     def saybye():
-        p('Bye!')
+        print('Bye!')
 
     FlaskUI(app=create_app(), server='flask', on_shutdown=saybye()).run()
