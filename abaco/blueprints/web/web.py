ac82aeb6230a78b9fff2516023288342ec96a218
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-10-02 00:44:23 -0400

    get currency and locale dynamically

--- abaco/blueprints/web/web.py
+++ abaco/blueprints/web/web.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 from flask_babel import gettext as _
 from flaskwebgui import close_application
 
-from abaco.constants import APP_NAME
+from abaco.constants import APP_NAME, COUNTRIES, CURRENCIES
 from abaco.database import database_exists, empty_user_config, get_user_config
 
 web = Blueprint('web', __name__)
@@ -13,11 +13,17 @@ def hello():
 
     if not database_exists() or empty_user_config():
 
-        data = {'title': _('Welcome!') + ' | ' + APP_NAME}
+        data = {
+            'title': _('Welcome!') + ' | ' + APP_NAME,
+            'countries': COUNTRIES,
+            'currencies': CURRENCIES,
+        }
         return render_template('welcome.html', data=data)
 
     data = get_user_config().all()[0]
     data['title'] = APP_NAME
+    data['countries'] = COUNTRIES
+    data['currencies'] = CURRENCIES
     return render_template('index.html', data=data)
 
 
--- abaco/constants.py
+++ abaco/constants.py
@@ -1,1 +1,15 @@
 APP_NAME = 'Abaco'
+
+COUNTRIES = [
+    {'locale': 'es_US', 'emoji': '🇺🇸', 'country': 'English (US)'},
+    {'locale': 'pt_BR', 'emoji': '🇧🇷', 'country': 'Portuguese (BR)'},
+]
+
+CURRENCIES = [
+    {
+        'code_iso_4217': 'USD',
+        'emoji': '🇺🇸',
+        'currency': 'United States dollar',
+    },
+    {'code_iso_4217': 'BRL', 'emoji': '🇧🇷', 'currency': 'Brazilian real'},
+]
