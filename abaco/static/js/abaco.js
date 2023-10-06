925b38bc2edac46206a1219724a2e2a75fbfd901
William B M Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-10-06 13:07:36 -0400

    update chart

--- abaco/static/js/abaco.js
+++ abaco/static/js/abaco.js
@@ -1,11 +1,19 @@
+function treat_locale(locale) {
+    return String(locale).replace('_', '-')
+}
+
 function format_currency(value, locale, currency) {
     return new Intl.NumberFormat(
-        String(locale).replace('_', '-'),
+        treat_locale(locale),
         {
             style: 'currency',
             currency: String(currency).toUpperCase()
         }
     ).format(value)
+}
+
+function format_date(date, locale) {
+    return new Intl.DateTimeFormat(treat_locale(locale)).format(Date.parse(date))
 }
 
 function ajax_requester(url, data = null, method = 'POST', reload = true, success_callback = null) {
@@ -30,5 +38,15 @@ function ajax_requester(url, data = null, method = 'POST', reload = true, succes
         }
     }).fail((response) => {
         alert('Error: ' + response.responseJSON.message)
+    })
+}
+
+function create_chart(el, type, labels, datasets) {
+    return new Chart(el, {
+        type: type,
+        data: {
+            labels: labels,
+            datasets: datasets
+        }
     })
 }
