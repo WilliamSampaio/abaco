280e7fd6315b0f808cea808b62a4ca7975006164
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-05-21 02:59:45 -0400

    hello world

--- abaco/__init__.py
+++ abaco/__init__.py
@@ -1,29 +1,8 @@
-import os
-import platform
-
-from flask import Flask
-
-from abaco import localization
-from abaco.blueprints import api, web
-from abaco.constants import APP_ENV, APP_NAME
-
-base_dir = os.path.abspath(os.path.dirname(__file__))
-system = platform.platform()
+from .config import settings
 
 
 def create_app():
-    app = Flask(__name__)
-    if APP_ENV == 'development':
-        app.config['APP_NAME'] = APP_NAME + f' (Dev: {system})'
-    else:
-        app.config['APP_NAME'] = APP_NAME
-    app.config['BABEL_TRANSLATION_DIRECTORIES'] = os.path.join(
-        base_dir, 'translations'
-    )
-    localization.init_app(app)
-    api.init_app(app)
-    web.init_app(app)
-    return app
 
+    import streamlit as st
 
-app = create_app()
+    st.write(settings)
