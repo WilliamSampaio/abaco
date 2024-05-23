60899508c1bb9115c6453ec6fee7abd704cf76dc
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-05-23 01:36:31 -0400

    fix company_icon

--- abaco/webscraping.py
+++ abaco/webscraping.py
@@ -6,7 +6,7 @@ from .config import settings as s
 
 
 def company_icon(ticker: str, is_fii: bool = False):
-    url = s.INVESTIDOR10_URL2 if is_fii else s.INVESTIDOR10_URL1
+    url = s.STATUSINVEST_URL2 if is_fii else s.STATUSINVEST_URL1
     req = requests.get(
         str(url).format(ticker.lower()), headers={'User-Agent': 'Mozilla/5.0'}
     )
@@ -15,9 +15,9 @@ def company_icon(ticker: str, is_fii: bool = False):
     soup = BeautifulSoup(req.content, 'html.parser')
     dom = etree.HTML(str(soup))
     try:
-        uri = dom.xpath('//*[@id="header_action"]/div[1]/div[1]/img')[
-            0
-        ].attrib['src']
-        return str(s.INVESTIDOR10_URL_BASE + uri)
+        parentid = dom.xpath('//*[@id="btn-resume-wallet"]')[0].attrib[
+            'data-parentid'
+        ]
+        return str(s.STATUSINVEST_URL_STOCK_ICON).format(parentid)
     except Exception:
         return False
