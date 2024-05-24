099b107822b1b69b7c2ccc8216f25387093457f7
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2024-05-24 02:58:02 -0400

    update

--- abaco/pages/dashboard.py
+++ abaco/pages/dashboard.py
@@ -1,11 +1,36 @@
+import pandas as pd
+
+from abaco.database import Session
+from abaco.models import Negociacoes
+
 title = 'Dashboard'
 
 
 def page_dashboard():
 
+    session = Session()
+
     import streamlit as st
+
+    def patrimonio_dataframe():
+        dataset = []
+        negociacoes = (
+            session.query(Negociacoes).order_by(Negociacoes.data_pregao).all()
+        )
+        if len(negociacoes) > 0:
+            negociacoes.reverse()
+            for negociacao in negociacoes:
+                dataset.append(negociacao.__dict__)
+
+        df = pd.DataFrame(dataset)
+
+        st.write(df)
+
+        df = df.groupby(['data_pregao', 'movimentacao'])['valor_total'].sum()
+
+        st.write(df)
 
     st.write('# ' + title)
     st.divider()
 
-    st.write(st.session_state.wallet)
+    patrimonio_dataframe()
